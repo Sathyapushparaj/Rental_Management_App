0a3f0711af033bfd37db831cff93d6d2f93afc22
Sathyapushparaj <138609681+Sathyapushparaj@users.noreply.github.com>
2023-07-05 22:07:15 +0530

    Delete shopproject directory

--- shopproject/shopapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','image','description')
-
-# Register your models here.
-admin.site.register(Catagory,CategoryAdmin)
-admin.site.register(Product)
--- shopproject/shopapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path('main', views.main, name='main'),
-    path('register', views.register, name="register"),
-]
